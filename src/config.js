7f9997fb84a4b424ff9a460fb9f2cc37518ac09b
Gary Gu <gu.gary@gmail.com>
2018-07-15 19:21:31 -0400

    env

--- src/config.js
+++ src/config.js
@@ -1,17 +1,42 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-east-2",
+    BUCKET: "sls-api-dev-attachmentsbucket-1d6ippxpi7ewy"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-east-2",
+    URL: "https://cq6qo7q5n7.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_ftYJN6YTE",
+    APP_CLIENT_ID: "2etl65d6u2p5lsjrn1u9cmhkh1",
+    IDENTITY_POOL_ID: "us-east-2:9c7bbdf6-ebf7-4f68-8408-b17cec95e759"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "sls-api-prod-attachmentsbucket-4xzfr425ftol"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://2p33i2orw5.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_4XEuJDCIw",
+    APP_CLIENT_ID: "5oj8pekuem73gk4g9tr3jmee5",
+    IDENTITY_POOL_ID: "us-east-2:789dfa14-a1ca-47ff-8765-ee724d8ab38d"
+  }
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
